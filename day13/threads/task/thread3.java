48e45e6ff95c77f1463eedb08957e512a8168cb6
sonithshetty <sonit.harish.shetty@gmail.com>
2023-01-17 23:13:55 +0530

    Uploading task1 of day13 on threads

---
+++ day13/threads/task/thread3.java
@@ -0,0 +1,12 @@
+package day13.threads.task;
+
+public class thread3 {
+  public void run() {
+    for (int i = 0; i < 100; i++) {
+      System.out.println("Thread3" + i);
+    }
+  }
+
+  public void start() {
+  }
+}
---
+++ day13/threads/task/thread4.java
@@ -0,0 +1,12 @@
+package day13.threads.task;
+
+public class thread4 {
+  public void run() {
+    for (int i = 0; i < 100; i++) {
+      System.out.println("Thread4" + i);
+    }
+  }
+
+  public void start() {
+  }
+}
