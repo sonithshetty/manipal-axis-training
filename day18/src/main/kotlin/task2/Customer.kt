441b8356320a77e312313a992433bb1fc1a3804a
sonithshetty <sonit.harish.shetty@gmail.com>
2023-01-23 19:14:03 +0530

    Uploading task2 of day18 on kotlin

---
+++ day18/src/main/kotlin/task2/Customer.kt
@@ -0,0 +1,26 @@
+package task2
+
+class Customer {
+
+}
+fun main(){
+
+}
+//create class ticket
+// from
+// to
+// date
+// time
+// cost
+// prif
+// setno//
+// customer class
+// name
+// age
+// place
+// bookin class
+// customer deatils
+// CUSTOMER LIST
+// booking details
+
+// show the perticular booking details
