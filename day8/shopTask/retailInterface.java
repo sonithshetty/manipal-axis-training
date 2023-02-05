42b08d75985665de478ad5747eb0809355353013
Sonit Harish Shetty <86555430+sonithshetty@users.noreply.github.com>
2023-02-05 16:06:32 +0530

    Delete day8/shopTask directory

--- day8/shopTask/retailInterface.java
+++
@@ -1,8 +0,0 @@
-package day8.shopTask;
-
-public interface retailInterface {
-  float getTotalCost(float tax, float cost, float number);
-  float getTotalExtendedCost(float cosmetics, float perfume, float clothes);
-  void getDiscount(float totalCost);
-
-}
