31560f47c45b61f979722c970d267d197ca82d7a
sonithshetty <sonit.harish.shetty@gmail.com>
2023-02-05 15:44:22 +0530

    task uploaded on springboot

---
+++ day25/Spring-mysql/src/main/java/com/mysql/controller/TicketController.java
@@ -0,0 +1,30 @@
+package com.mysql.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mysql.dao.TicketDao;
+import com.mysql.model.Ticket;
+
+@RestController
+@RequestMapping("/ticket")
+public class TicketController {
+	@Autowired
+	private TicketDao dao;
+	
+	@PostMapping("/bookTickets")
+	public String bookTicket(@RequestBody  List<Ticket> tickets) {
+		dao.saveAll(tickets);
+		return "ticket booked: " + tickets.size();
+	}
+	@GetMapping("/getTickets")
+	public List<Ticket> getTickets(){
+		return (List<Ticket>) dao.findAll(); 
+	}
+}
