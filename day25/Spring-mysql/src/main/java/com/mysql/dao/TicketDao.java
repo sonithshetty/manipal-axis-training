9740d5a9fc92ad37a0b9be922beccacf05277b05
Sonit Harish Shetty <86555430+sonithshetty@users.noreply.github.com>
2023-02-05 16:20:21 +0530

    Delete day25/Spring-mysql directory

--- day25/Spring-mysql/src/main/java/com/mysql/dao/TicketDao.java
+++
@@ -1,9 +0,0 @@
-package com.mysql.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.mysql.model.Ticket;
-
-public interface TicketDao extends CrudRepository<Ticket, Integer>{
-
-}
