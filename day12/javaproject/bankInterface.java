fb7f9a31bb334c49d79af14c4ec0fbcebf698098
Sonit Harish Shetty <86555430+sonithshetty@users.noreply.github.com>
2023-02-05 16:10:52 +0530

    Delete day12/javaproject directory

--- day12/javaproject/bankInterface.java
+++
@@ -1,9 +0,0 @@
-package day12.javaproject;
-
-public interface bankInterface {
-  final String name = "ABC";
-  final String bankCode = "ABC2001";
-  final String bankAddress = "Kandarpada, Dahisar";
-
-  void getAccount();
-}
